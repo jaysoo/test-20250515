9cbc9392e3580b107605f724a0479367ecb5bea2
Jack Hsu <jack.hsu@gmail.com>
2025-05-17 09:28:26 -0400

    add example of e2e

--- server.cjs
+++ server.cjs
@@ -2,11 +2,11 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('hi');
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hello</h1><p>Welcome to the server!</p>');
 });
 
-const PORT = 3000;
+const PORT = process.argv[2] ? parseInt(process.argv[2]) : 3000;
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
 });
